36ebf46fb08226107ff1374edd4838bc7468a578
Jaili Martins <114961073+jailimartins96@users.noreply.github.com>
2025-12-28 21:55:23 -0300

    Implementações usando Bootstrap

--- project/script.js
+++ project/script.js
@@ -2,7 +2,8 @@ const inputText = document.getElementById("inputtext");
 const botaoGerar = document.getElementById("botaoGerar");
 const resultado = document.getElementById("resultado");
 
-
+const qrArea = document.getElementById("qrArea");
+const qrImagem = document.getElementById("qrImagem");
 
 botaoGerar.addEventListener("click", function () {
 
@@ -11,14 +12,19 @@ botaoGerar.addEventListener("click", function () {
     if (valorDigitado === "") {
         mostrarAlerta("Digite algum texto para gerar o QR Code.", "erro");
         resultado.innerText = "";
+        qrImagem.src = "";
+        qrArea.classList.add("oculto");
         return;
     }
 
     mostrarAlerta("Texto válido! QR Code será gerado.", "sucesso");
     resultado.innerText = valorDigitado;
-    return;
-});
+    const urlQR = "https://quickchart.io/qr?size=200&text=" +
+        encodeURIComponent(valorDigitado);
 
+    qrImagem.src = urlQR;
+    qrArea.classList.remove("oculto");
+});
 
 
 
@@ -28,3 +34,6 @@ function mostrarAlerta(mensagem, tipo) {
     alerta.innerText = mensagem;
     alerta.className = `alerta ${tipo}`;
 }
+
+
+mostrarAlerta("Bem-vindo ao Gerador de QR Code!", "sucesso");
